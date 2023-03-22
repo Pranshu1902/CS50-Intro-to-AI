735418dbdd1a3c84f32374da6aa44178647a14ea
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-03-23 00:47:29 +0530

    complete pagerank project

--- Lecture 2 Uncertainity/pagerank/pagerank.py
+++ Lecture 2 Uncertainity/pagerank/pagerank.py
@@ -57,7 +57,26 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+
+    transition_probs = dict.fromkeys(corpus.keys(), 0)
+    if len(corpus[page]) > 0:
+        N = len(corpus[page])
+        probability = damping_factor / N
+        for page in corpus[page]:
+            transition_probs[page] = probability
+    else:
+        # if the page doesn't contain outgoing links
+        # we'll assume it has links to all pages thus the probability
+        N = len(corpus)
+        probability = damping_factor / N
+        for page in corpus:
+            transition_probs[page] = probability
+
+    # giving probality to all the pages of the corpus using value of 1 - damping_factor 
+    N = len(corpus)
+    probability = (1 - damping_factor) / N
+    transition_probs = {k:v+probability for (k, v) in transition_probs.items()}
+    return transition_probs
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,7 +88,23 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    distribution = {}
+    for page in corpus.keys():
+        distribution[page] = 0.0
+    
+    page = random.choice(list(corpus.keys()))
+
+    for i in range(1, n):
+        # storing the new distribution
+        current_distribution = transition_model(corpus, page, damping_factor)
+        for page in distribution:
+            # formula of adding new probability to previous probability
+            distribution[page] = ((i-1) * distribution[page] + current_distribution[page]) / i
+
+        # selecting the next random page including the ones that can't be reached from current page
+        page = random.choices(list(distribution.keys()), list(distribution.values()), k=1)[0]
+
+    return distribution
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -81,7 +116,31 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    ranks = {}
+    threshold = 0.0005
+    N = len(corpus)
+    
+    for key in corpus:
+        ranks[key] = 1 / N
+
+
+    while True:
+        count = 0
+        for key in corpus:
+            new = (1 - damping_factor) / N
+            sigma = 0
+            for page in corpus:
+                if key in corpus[page]:
+                    num_links = len(corpus[page])
+                    sigma = sigma + ranks[page] / num_links
+            sigma = damping_factor * sigma
+            new += sigma
+            if abs(ranks[key] - new) < threshold:
+                count += 1
+            ranks[key] = new 
+        if count == N:
+            break
+    return ranks
 
 
 if __name__ == "__main__":
